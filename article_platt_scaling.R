5845e0278af48daddcfb1ed83beff4971cbfafc7
Neeraj Singh Sarwan <nsingh.sarwan.min13@itbhu.ac.in>
2016-07-07 15:09:27 +0530

    added log loss and reliability plot code

--- article_platt_scaling.R
+++ article_platt_scaling.R
@@ -2,6 +2,17 @@ setwd("F:\\Internship_AV\\Datasets\\Blood_Donation")
 
 list.files()
 
+MultiLogLoss<-function(act, pred)
+{
+  eps = 1e-15;
+  nr = length(pred)
+  pred = matrix(sapply( pred, function(x) max(eps,x)), nrow = nr)      
+  pred = matrix(sapply( pred, function(x) min(1-eps,x)), nrow = nr)
+  ll = sum(act*log(pred) + (1-act)*log(1-pred))
+  ll = ll * -1/(length(act))      
+  return(ll);
+}
+
 train<-read.csv("train.csv")
 test<-read.csv("test.csv")
 
@@ -41,6 +52,43 @@ result_cv_platt<-predict(model_log,dataframe[-2],type = "response")
 MultiLogLoss(as.numeric(as.character(cv$Made.Donation.in.March.2007)),result_cv_platt)
 
 # plotting reliability plots
+
+reliability.plot <- function(obs, pred, bins=10, scale=T) {
+  #  Plots a reliability chart and histogram of a set of predicitons from a classifier
+  #
+  # Args:
+  #   obs: Vector of true labels. Should be binary (0 or 1)
+  #   pred: Vector of predictions of each observation from the classifier. Should be real
+  #       number
+  #   bins: The number of bins to use in the reliability plot
+  #   scale: Scale the pred to be between 0 and 1 before creating reliability plot
+  require(plyr)
+  library(Hmisc)
+  
+  min.pred <- min(pred)
+  max.pred <- max(pred)
+  min.max.diff <- max.pred - min.pred
+  
+  if (scale) {
+    pred <- (pred - min.pred) / min.max.diff 
+  }
+  
+  bin.pred <- cut(pred, bins)
+  
+  k <- ldply(levels(bin.pred), function(x) {
+    idx <- x == bin.pred
+    c(sum(obs[idx]) / length(obs[idx]), mean(pred[idx]))
+  })
+  
+  is.nan.idx <- !is.nan(k$V2)
+  k <- k[is.nan.idx,]  
+  
+  
+  #subplot(hist(pred, xlab="", ylab="", main="", xlim=c(0,1), col="blue"), grconvertX(c(.8, 1), "npc"), grconvertY(c(0.08, .25), "npc"))
+  return(k)
+}
+
+
 plot(c(0,1),c(0,1), col="grey",type="l",xlab = "Mean Prediction",ylab="Observed Fraction")
 k<-reliability.plot(as.numeric(as.character(cv$Made.Donation.in.March.2007)),result_cv$`1`,bins = 5)
 lines(k$V2, k$V1, xlim=c(0,1), ylim=c(0,1), xlab="Mean Prediction", ylab="Observed Fraction", col="red", type="o", main="Reliability Plot")
